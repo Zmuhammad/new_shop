70d3a5b0b7c65155849f13132687353a39cb9d71
Zmuhammad <zareeimuhammad52@gmail.com>
2025-08-04 20:13:22 +0330

    debug mode enable

--- A/settings.py
+++ A/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t^or3tom*^jq_m_p&!9t1w0z!s01qyo88d1cy@+e^z_*140w7c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
